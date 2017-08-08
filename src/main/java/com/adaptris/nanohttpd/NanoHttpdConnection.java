3e8032c4463d5da69624ff26b878d5689a15bf75
Lewin Chan <lewin.chan@adaptris.com>
2017-08-08 13:01:23 +0100

    Added minimal set of stuffs for Stuart to start work; copied from hpcc.

---
+++ src/main/java/com/adaptris/nanohttpd/NanoHttpdConnection.java
@@ -0,0 +1,11 @@
+
+package com.adaptris.nanohttpd;
+
+import com.adaptris.core.*;
+import com.adaptris.core.util.*;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("nano-httpd-connection")
+public abstract class NanoHttpdConnection extends AdaptrisConnectionImp {
+}
---
+++ src/test/java/com/adaptris/nanohttpd/NanoHttpdConnectionTest.java
@@ -0,0 +1,19 @@
+package com.adaptris.nanohttpd;
+
+import com.adaptris.core.*;
+
+public class NanoHttpdConnectionTest extends BaseCase {
+
+  public NanoHttpdConnectionTest(java.lang.String testName) {
+    super(testName);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+  }
+
+  public void testSomething() throws Exception {
+    fail("What is this something?");
+  }
+
+}
